72759a0327ab09d68eb5ac239dae413e03e9a34c
Krish Bagga <kbagga@myseneca.ca>
2025-05-24 04:09:41 -0400

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Author: Krish Bagga
+# Author ID: 148191232
+# Date Created: 2025/05/24
+
+import sys
+
+timer = int(sys.argv[1]) 
+while timer != 0:
+    print (timer)
+    timer = timer - 1
+print("blast off!")
+
